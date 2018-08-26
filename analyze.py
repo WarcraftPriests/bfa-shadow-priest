41c6d88bb5319762191d8c1d1d25081f4ff29932
djriff <riff.ibanezius@gmail.com>
2018-08-26 13:39:17 -0700

    Updated new path to CSV converter

--- analyze.py
+++ analyze.py
@@ -170,4 +170,4 @@ with open(outputCSV, 'w') as resultsCSV:
 #Update JSONs
 import subprocess
 if args.dir == "trinkets/" or "traits/" or "azerite-gear/":
-	subprocess.call("json_Charts/csvToJson.py")
+	subprocess.call("csvToJson.py")
